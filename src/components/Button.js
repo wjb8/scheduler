afc116f242d2e8edbbd9ddbd0d279f3d54b32f4c
chinny36 <brownj95@gmail.com>
2021-05-10 14:39:35 -0400

    First activity conditional CSS

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,26 @@
 import React from "react";
-
+import classNames from "classnames";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
-}
+   let buttonClass = classNames("button", {
+      "button--confirm" : props.confirm,
+      "button--danger" : props.danger,
+      "button--disabled" : props.disabled,
+      "button--clickable" : props.clickable
+   });
+
+    if (props.disabled) {
+      buttonClass += " button--disabled";
+    }
+ 
+   return (
+      <button 
+         className={buttonClass}
+         onClick={props.onClick}
+         disabled={props.disabled}
+      >
+         {props.children}
+      </button>
+      );
+ }
