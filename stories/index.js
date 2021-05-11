a50c4f15b0d3dab0ca390f1d05a0a1ae16439e4c
chinny36 <brownj95@gmail.com>
2021-05-11 17:08:54 -0400

    Add Empty and Show components

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from "components/InterviewerList"
 import Appointment from "components/Appointment/index"
 import Header from "components/Appointment/Header"
 import Empty from "components/Appointment/Empty"
+import Show from "components/Appointment/Show"
 
 const days = [
   {
@@ -141,3 +142,4 @@ storiesOf("Button", module)
       .add("Appointment with Time", () => <Appointment time="12pm" />)
       .add("Header", () => <Header time="12pm" />)
       .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+      .add("Show", () => <Show student="Heavens Murgatroyd" interviewer={interviewer.name} onEdit={action("onEdit")} onDelete={action("onDelete")} />)
