2830e06c287f7c5a46768766c425bb0167a903eb
chinny36 <brownj95@gmail.com>
2021-05-12 14:37:08 -0400

    Implement days api for days data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "components/DayList"
 import Appointment from "components/Appointment"
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -78,8 +61,11 @@ export default function Application(props) {
   )
 
   const [day, setDay] = useState('Monday');
+  const [days, setDays] = useState([]);
 
-  // const listAppointments = 
+  useEffect(() => {
+    axios.get(`http://localhost:8001/api/days`).then(response => setDays(response.data))
+  })
 
   return (
     <main className="layout">
