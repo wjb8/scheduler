92d9c1a3f7cc620706807e9cc35eae621858d503
chinny36 <brownj95@gmail.com>
2021-05-11 22:43:55 -0400

    Begin iterating over appointments task

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "components/DayList"
+import Appointment from "components/Appointment"
 
 const days = [
   {
@@ -21,8 +22,57 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "7pm",
+    interview: {
+      student: "Sacka Wacka",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "3pm",
+  },
+  {
+    id: 5,
+    time: "12pm",
+    interview: {
+      student: "Bob Mould",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  }
+];
+
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
+
+  // const listAppointments = 
 
   return (
     <main className="layout">
@@ -47,7 +97,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        
       </section>
     </main>
   );
