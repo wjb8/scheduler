c94d3449cde5ce9f88e445c8baf7acbedb66f84b
chinny36 <brownj95@gmail.com>
2021-05-12 15:21:03 -0400

    Add getAppointmentsForDay helper

---
+++ src/helpers/selectors.js
@@ -0,0 +1,14 @@
+export function getAppointmentsForDay(state, day) {
+  const appointmentsForDay = [];
+  const dayFound = state.days.find(el => el.name === day);
+  
+  if (!dayFound) {
+    return [];
+  }
+
+  dayFound.appointments.forEach(app => {
+    appointmentsForDay.push(state.appointments[app])
+  });
+  
+  return appointmentsForDay;
+};
