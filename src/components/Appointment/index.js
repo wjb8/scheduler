871c9bdedb85de24d8525864cd4612eb21782d38
chinny36 <brownj95@gmail.com>
2021-05-14 17:29:19 -0400

    Add confirm

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,6 +4,7 @@ import Show from "components/Appointment/Show"
 import Empty from "components/Appointment/Empty"
 import Form from "components/Appointment/Form"
 import Status from "components/Appointment/Status"
+import Confirm from "components/Appointment/Confirm"
 import useVisualMode from "hooks/useVisualMode"
 import "components/Appointment/styles.scss"
 
@@ -14,6 +15,7 @@ export default function Appointment(props) {
   const CREATE = "CREATE";
   const SAVING = "SAVING";
   const DELETING = "DELETING";
+  const CONFIRM = "CONFIRM";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
@@ -35,6 +37,10 @@ export default function Appointment(props) {
     .then(res => transition(EMPTY));
   }
 
+  const confirm = () => {
+    transition(CONFIRM);
+  }
+
   return (
     <article className="appointment">
       <Header time={props.time}></Header>
@@ -43,7 +49,7 @@ export default function Appointment(props) {
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-          onDelete={cancel}
+          onDelete={confirm}
         />
       )}
       {mode === CREATE && (
@@ -61,6 +67,13 @@ export default function Appointment(props) {
       {mode === DELETING && (
         <Status 
           message="Deleting..."
+        />
+      )}
+      {mode === CONFIRM && (
+        <Confirm 
+          message="Delete appointment?"
+          onConfirm={cancel}
+          onCancel={back}
         />
       )}
     </article>
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,38 +3,6 @@ import { useState } from 'react';
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
-  // const state = {
-  //   elements: [initial],
-  //   currentElement: initial
-  // };
-
-  // const push = (newEl, replace) => {
-    
-  //   state.currentElement = newEl;
-
-  //   if (replace) {
-  //     state.elements = state.elements.slice(0,-1);
-  //   }
-
-  //   state.elements = [...state.elements, newEl];
-
-  // };
-
-  // const pop = () => {
-
-  //   state.elements = state.elements.slice(0,-1);
-  //   state.currentElement = state.elements[state.elements.length - 1];
-
-  // };
-
-  // const replace = (el) => {
-
-  //   state.elements = state.elements.slice(0,-1);
-  //   state.elements = [...state.elements, el];
-  //   state.currentElement = el;
-
-  // }
-
   const transition = (newMode, replace = false) => {
     
     if (replace) {
@@ -47,7 +15,6 @@ export default function useVisualMode(initial) {
 
   const back = () => {
     if (history.length > 1) {
-      console.log('inside if')
       history.pop();
       setMode(history[history.length - 1])
     } 
